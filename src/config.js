0e830e394d3a05fdddb1a5cb88b1259f90cb9928
quillkong <29533911+quillkong@users.noreply.github.com>
2020-02-08 00:10:05 +0800

    v1

---
+++ src/config.js
@@ -0,0 +1,36 @@
+var introText = 'Over 110 sub-centres are being planned in Chinese metropolises with a population over 5 million. They have been conceived as major new cores for rebalancing economic and social development, which all too often tends to concentrate on the historic centres. China’s urbanisation needs such new centres as drivers for sustainable growth, yet successful realisation of the planned vision is one of the hardest acts of planning to pull off.'
+
+
+var option_senario = [{
+  value: 's1',
+  label: '情景1',
+  children: [
+    {
+    value: 'd',
+    label: '情景1.1'
+    }
+  ]
+  },
+  {
+    value: 's2',
+    label: '情景2'
+    }
+]
+var option_output = [{
+          value: 'Pop',
+          label: 'Population'
+        }, {
+          value: 'Hh',
+          label: 'Hh'
+        }]
+var option_year = [{
+          value: '00',
+          label: '2000'
+        }, {
+          value: '10',
+          label: '2010'
+        }, {
+        value: '15',
+        label: '2015'
+      }]
+var layerList = {'d_15_Pop': [1149, 8913965], 'd_10_Pop': [202, 9122102], 'd_00_Pop': [512.0, 441421.0], 'd_00_Hh': [405, 2504337], 'd_10_Hh': [168, 3365134], 'd_15_Hh': [406.0, 209246.0]}
